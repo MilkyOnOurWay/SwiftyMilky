310207ec8a2c2b270d6846a884831238b0f1f0dc
yoonjinlee <yoonjinifer@gmail.com>
2020-12-30 05:13:49 +0900

    🎨탭 바 연결 및 기초 세팅 완료

---
+++ Milkyway/Milkyway/Screens/Home/ViewControllers/HomeVC.swift
@@ -0,0 +1,17 @@
+//
+//  HomeVC.swift
+//  Milkyway
+//
+//  Created by 이윤진 on 2020/12/30.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+    }
+    
+    
+}
---
+++ Milkyway/Milkyway/Screens/TabBar/TabBarController.swift
@@ -0,0 +1,24 @@
+//
+//  TabBarController.swift
+//  Milkyway
+//
+//  Created by 이윤진 on 2020/12/30.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+}
+
+extension TabBarController {
+    
+
+    
+}

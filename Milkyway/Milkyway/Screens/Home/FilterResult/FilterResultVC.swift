e3ac97807deb9c85281bb28c6c83053949189d2c
yoonjinlee <yoonjinifer@gmail.com>
2020-12-31 00:45:47 +0900

    🚀네이버지도 API 추가

---
+++ Milkyway/Milkyway/Screens/Home/FilterResult/FilterResultVC.swift
@@ -0,0 +1,18 @@
+//
+//  FilterResultVC.swift
+//  Milkyway
+//
+//  Created by 이윤진 on 2020/12/31.
+//
+
+import UIKit
+
+class FilterResultVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     
+        // Do any additional setup after loading the view.
+    }
+    
+}

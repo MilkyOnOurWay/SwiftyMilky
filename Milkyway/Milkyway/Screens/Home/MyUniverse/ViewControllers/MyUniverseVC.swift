5be53dbfaa750acb80407ed3782b48b8e978b8dd
yoonjinlee <yoonjinifer@gmail.com>
2021-01-04 02:50:42 +0900

    ⚡️[윤진] 탭 바 수정(유니버스 메뉴 추가)

--- Milkyway/Milkyway/Screens/Home/MyUniverse/ViewControllers/MyUniverseVC.swift
+++
@@ -1,8 +0,0 @@
-//
-//  MyUniverseVC.swift
-//  Milkyway
-//
-//  Created by 이윤진 on 2020/12/30.
-//
-
-import Foundation
---
+++ Milkyway/Milkyway/Screens/MyUniverse/ViewControllers/MyUniverseVC.swift
@@ -0,0 +1,17 @@
+//
+//  MyUniverseVC.swift
+//  Milkyway
+//
+//  Created by 이윤진 on 2020/12/30.
+//
+
+import UIKit
+
+class MyUniverseVC: UIViewController{
+    
+    override func viewDidLoad(){
+        
+        super.viewDidLoad()
+        
+    }
+}

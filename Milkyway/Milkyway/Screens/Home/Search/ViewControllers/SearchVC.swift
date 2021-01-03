70cbb85b1dbe3ffdcc4da2e902fd165b7f3c172c
yoonjinlee <yoonjinifer@gmail.com>
2021-01-03 22:42:51 +0900

    🎨 [윤진] 검색화면 레이아웃 구성

--- Milkyway/Milkyway/Screens/Home/Search/ViewControllers/SearchVC.swift
+++ Milkyway/Milkyway/Screens/Home/Search/ViewControllers/SearchVC.swift
@@ -6,18 +6,61 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
 class SearchVC: UIViewController {
 
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var searchButton: UIButton!
+    @IBOutlet weak var searchTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setSearchTableView()
+        setBackButton()
     }
     
+}
 
-    @IBAction func backBtnClicked(_ sender: Any) {
+extension SearchVC {
+    
+    func setSearchTableView(){
+        searchTableView.delegate = self
+        searchTableView.dataSource = self
+        let nibName = UINib(nibName: "SearchTVC", bundle: nil)
+        searchTableView.register(nibName, forCellReuseIdentifier: "SearchTVC")
+    }
+    func setBackButton(){
+        backButton.addTarget(self, action:#selector(backBtnClicked), for: .touchUpInside)
+    }
+    
+    @objc func backBtnClicked(){
         
         navigationController?.popViewController(animated: true)
     }
+}
+
+extension SearchVC: UITableViewDelegate {
+
+}
+
+extension SearchVC: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = searchTableView.dequeueReusableCell(withIdentifier: "SearchTVC",for: indexPath) as! SearchTVC
+        cell.cafeNameLabel.text = "카페이름"
+        cell.cafeAddressLabel.text = "카페주소"
+        cell.cafeNameLabel.sizeToFit()
+        cell.cafeAddressLabel.sizeToFit()
+        return cell
+    }
+    
+    
     
 }

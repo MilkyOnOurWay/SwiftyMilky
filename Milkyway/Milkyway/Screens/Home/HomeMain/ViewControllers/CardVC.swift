44d20f14bab6806afd4964e978bc75a04a2f980d
yujin <glisteneugene@gmail.com>
2021-01-04 17:58:24 +0900

    🎨 [유진] 바텀시트 레이아웃

---
+++ Milkyway/Milkyway/Screens/Home/HomeMain/ViewControllers/CardVC.swift
@@ -0,0 +1,25 @@
+//
+//  CardVC.swift
+//  Milkyway
+//
+//  Created by ✨EUGENE✨ on 2021/01/04.
+//
+
+import UIKit
+
+class CardVC: UIViewController {
+
+    @IBOutlet var handleArea: UIView!
+    @IBOutlet var handleBar: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setHandler()
+    }
+
+    func setHandler() {
+//        handleArea.layer.cornerRadius = 12
+        handleBar.layer.cornerRadius = handleBar.frame.height / 2
+    }
+
+}

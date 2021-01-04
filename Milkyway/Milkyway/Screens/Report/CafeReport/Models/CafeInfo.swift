2ee5a9d77f980137ef6ca1bb0294ea3a30d9b161
소영 <sso_0022@naver.com>
2021-01-04 21:07:08 +0900

    🎨 카페제보뷰 - 레이아웃 초안

---
+++ Milkyway/Milkyway/Screens/Report/CafeReport/Models/CafeInfo.swift
@@ -0,0 +1,13 @@
+//
+//  CafeInfo.swift
+//  Milkyway
+//
+//  Created by soyounglee on 2021/01/04.
+//
+
+import Foundation
+
+struct CafeInfo {
+    var cafeName: String = ""
+    var cafeAddress: String = ""
+}
---
+++ Milkyway/Milkyway/Screens/Report/CafeReport/Models/CafeMenu.swift
@@ -0,0 +1,15 @@
+//
+//  CafeMenu.swift
+//  Milkyway
+//
+//  Created by soyounglee on 2021/01/04.
+//
+
+import UIKit
+
+struct CafeMenu {
+    var menu: String = ""
+    var selection = [Int]()
+    var price: String = ""
+    
+}

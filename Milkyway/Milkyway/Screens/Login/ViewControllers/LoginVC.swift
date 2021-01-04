ac4123148c6440e3bd2fbea30f9e420606f2e139
yoonjinlee <yoonjinifer@gmail.com>
2021-01-05 02:18:32 +0900

    ⚡️[윤진] 로그인 수정 진행 중

--- Milkyway/Milkyway/Screens/Login/ViewControllers/LoginVC.swift
+++ Milkyway/Milkyway/Screens/Login/ViewControllers/LoginVC.swift
@@ -12,13 +12,17 @@ class LoginVC: UIViewController{
     @IBOutlet weak var startButton: UIButton!
     @IBOutlet weak var nicknameTextField: UITextField!
     @IBOutlet weak var stateLabel: UILabel!
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var checkBoxImageView: UIImageView!
+    
+    var count = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         setTextfield()
+        setCheckBox()
         setButton()
-        
+        setLabel()
         // Do any additional setup after loading the view.
     }
     
@@ -27,10 +31,17 @@ class LoginVC: UIViewController{
 
 extension LoginVC {
     
+    func setLabel(){
+        
+        stateLabel.isHidden = true
+    }
     func setTextfield(){
         
         nicknameTextField.delegate = self
         nicknameTextField.addTarget(self, action: #selector(checkRegister), for: .editingChanged)
+    }
+    func setCheckBox(){
+        checkBoxImageView.isHidden = true
     }
     
     // MARK: - 로그인 완료 버튼 눌렀을 때 액션
@@ -43,6 +54,7 @@ extension LoginVC {
         
         let nicknameRegEx = "[가-힣0-9]{2,10}"
         let nicknameTest = NSPredicate(format: "SELF MATCHES %@", nicknameRegEx)
+        
         return nicknameTest.evaluate(with: nickname)
     }
     
@@ -60,14 +72,38 @@ extension LoginVC: UITextFieldDelegate {
     @objc func checkRegister(_ textfield: UITextField) {
         
         let nickname = textfield.text
-        if !isValidNickname(nickname!){
+        //변경전
+//        if !isValidNickname(nickname!){
+//            stateLabel.isHidden = false
+//            stateLabel.text = "영어, 특수문자, 이모티콘은 사용 불가합니다."
+//            countLabel.text = String(nicknameTextField.text?.count ?? 0)
+//
+//            return
+//        } else {
+//
+//            return
+//        }
+        //변경후
+        while nicknameTextField.text?.count ?? 0 < 11 {
             
-            stateLabel.text = "영어, 특수문자, 이모티콘은 사용 불가합니다."
-            return
-        } else{
-            
-            stateLabel.text = "사용가능한 닉네임입니다."
+            if !isValidNickname(nickname!) {
+                
+                stateLabel.isHidden = false
+                stateLabel.text = "영어, 특수문자, 이모티콘은 사용 불가합니다."
+                countLabel.text = String(nicknameTextField.text?.count ?? 0)
+                
+                
+                return
+            } else {
+                
+                return
+            }
         }
+        
+    }
+    
+    @objc func textFieldDidChange(){
+        
         
     }
     

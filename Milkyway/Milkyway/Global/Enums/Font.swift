c9a34ba208241f141a1ab32f33ed31a6d53a605c
yoonjinlee <yoonjinifer@gmail.com>
2021-01-04 22:14:27 +0900

    🚀 [윤진] 폰트 추가

--- Milkyway/Milkyway/Global/Enums/Font.swift
+++ Milkyway/Milkyway/Global/Enums/Font.swift
@@ -9,11 +9,11 @@ import UIKit
 
 enum Font {
     
+    // MARK: - 제플린 속성 넘어오면 라벨 폰트 및 크기 설정 가능
+    
     static let emptyTableBoldLabel = UIFont.boldSystemFont(ofSize: 18)
     static let emptyTableLabel = UIFont.systemFont(ofSize: 16)
-
     static let chatLabel = UIFont.systemFont(ofSize: 13, weight: .regular)
     static let chatNameLabel = UIFont.systemFont(ofSize: 13, weight: .semibold)
-
-
+    
 }

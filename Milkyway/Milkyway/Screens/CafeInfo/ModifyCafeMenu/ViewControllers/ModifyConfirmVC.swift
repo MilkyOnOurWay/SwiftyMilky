c50e29fecd84370e3049d5a40af7fd78aa445bb5
yujin <glisteneugene@gmail.com>
2021-01-05 17:11:40 +0900

    💄[유진] 정보수정페이지 에셋 오토

--- Milkyway/Milkyway/Screens/CafeInfo/ModifyCafeMenu/ViewControllers/ModifyConfirmVC.swift
+++ Milkyway/Milkyway/Screens/CafeInfo/ModifyCafeMenu/ViewControllers/ModifyConfirmVC.swift
@@ -11,12 +11,16 @@ class ModifyConfirmVC: UIViewController {
 
     @IBOutlet var rootView: UIView!
     @IBOutlet var confirmView: UIView!
+    
+    @IBOutlet var mainLabel: UILabel!
+    @IBOutlet var subLabel: UILabel!
     @IBOutlet var confirmBtn: UIButton!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setConfirmView()
+        setLabel()
         setButton()
     }
     
@@ -27,18 +31,29 @@ class ModifyConfirmVC: UIViewController {
             presentingVC?.navigationController?.popToRootViewController(animated: false)
         }
     }
+}
 
+extension ModifyConfirmVC {
     func setConfirmView() {
         rootView.backgroundColor = UIColor(displayP3Red: 0/255, green: 0/255, blue: 0/255, alpha: 0.7)
-        
         confirmView.layer.cornerRadius = 15
     }
     
+    func setLabel() {
+        mainLabel.textAlignment = .center
+        mainLabel.text = "요청이 접수 되었습니다"
+        mainLabel.font = UIFont(name:"SFProText-Bold", size: 16.0)
+        
+        subLabel.textAlignment = .center
+        subLabel.numberOfLines = 2
+        subLabel.text = "꼼꼼히 검토 후 적용하도록 하겠습니다\n감사합니다!"
+        subLabel.font = UIFont(name:"SFProText-Regular", size: 12.0)
+        subLabel.textColor = UIColor(displayP3Red: 80/255, green: 85/255, blue: 92/255, alpha: 1)
+    }
     func setButton() {
         confirmBtn.layer.cornerRadius = confirmBtn.frame.height / 2
         confirmBtn.setTitle("확인", for: .normal)
         confirmBtn.setTitleColor(.white, for: .normal)
         confirmBtn.backgroundColor = UIColor(named: "Milky")
     }
-
 }
--- Milkyway/Milkyway/Screens/CafeInfo/ModifyCafeMenu/ViewControllers/ModifyVC.swift
+++ Milkyway/Milkyway/Screens/CafeInfo/ModifyCafeMenu/ViewControllers/ModifyVC.swift
@@ -10,17 +10,21 @@ import UIKit
 class ModifyVC: UIViewController {
 
     
+    @IBOutlet var mainLabel: UILabel! //수정할 정보를 ~
+    @IBOutlet var subLabel: UILabel! // 최소 5글자
+    
     @IBOutlet var modifyTextView: UITextView!
     @IBOutlet var submitBtn: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setLabel()
         setTextView()
         setButton()
     }
     
     @IBAction func submitBtnClicked(_ sender: Any) {
-        print("Home - submitBtnClicked")
+        print("Modify - submitBtnClicked")
         
         guard let confirmVC = UIStoryboard(name: "Modify", bundle: nil).instantiateViewController(withIdentifier:"ModifyConfirmVC") as? ModifyConfirmVC else {
             return
@@ -28,15 +32,31 @@ class ModifyVC: UIViewController {
         confirmVC.modalPresentationStyle = .overCurrentContext
         present(confirmVC, animated: false, completion: nil)
     }
+}
+
+extension ModifyVC {
+    
+    func setLabel() {
+        mainLabel.textAlignment = .center
+        mainLabel.text = "수정할 정보를 작성해주세요"
+        mainLabel.font = UIFont(name:"SFProText-Bold", size: 15.0)
+        
+        subLabel.textAlignment = .left
+        subLabel.text = "*최소 5글자 이상"
+        subLabel.font = UIFont(name:"SFProText-Regular", size: 12.0)
+        subLabel.textColor = UIColor(named: "darkGrey")
+    }
     
     func setTextView() {
         modifyTextView.delegate = self
+        
         modifyTextView.text = "ex. 두유 밖에 팔지 않아요.\n콜드브루만 디카페인이래요."
-        modifyTextView.textColor = UIColor.lightGray
+        modifyTextView.font = UIFont(name:"SFProText-Regular", size: 16.0)
+        modifyTextView.textColor = .gray
 
         modifyTextView.backgroundColor = UIColor(displayP3Red: 246/255, green: 245/255, blue: 242/255, alpha: 1)
         modifyTextView.layer.cornerRadius = 10
-        modifyTextView.textContainerInset = UIEdgeInsets(top: 16, left: 14, bottom: 16, right: 14);
+        modifyTextView.textContainerInset = UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16);
     }
 
     func setButton() {
@@ -44,16 +64,14 @@ class ModifyVC: UIViewController {
         submitBtn.layer.cornerRadius = submitBtn.frame.height / 2
         submitBtn.setTitle("정보 수정 요청", for: .normal)
         submitBtn.setTitleColor(.white, for: .normal)
-        submitBtn.backgroundColor = .darkGray
+        submitBtn.backgroundColor = UIColor(named: "darkGrey")
     }
-
-
 }
 
 extension ModifyVC: UITextViewDelegate {
     // TextView Place Holder
     func textViewDidBeginEditing(_ textView: UITextView) {
-        if textView.textColor == UIColor.lightGray {
+        if textView.textColor == UIColor.gray {
             textView.text = nil
             textView.textColor = UIColor.black
         }
@@ -62,7 +80,7 @@ extension ModifyVC: UITextViewDelegate {
     func textViewDidEndEditing(_ textView: UITextView) {
         if textView.text.isEmpty {
             textView.text = "ex. 두유 밖에 팔지 않아요.\n콜드브루만 디카페인이래요."
-            textView.textColor = UIColor.lightGray
+            textView.textColor = UIColor.gray
         }
     }
     func textViewDidChange(_ textView: UITextView) {
@@ -71,7 +89,7 @@ extension ModifyVC: UITextViewDelegate {
             submitBtn.backgroundColor = UIColor(named: "Milky")
         } else {
             submitBtn.isEnabled = false
-            submitBtn.backgroundColor = .darkGray
+            submitBtn.backgroundColor = UIColor(named: "darkGrey")
         }
     }
 

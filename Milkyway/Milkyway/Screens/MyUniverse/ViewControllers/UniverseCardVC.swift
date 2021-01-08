f5da60b0d6dcfb7ed54760c9c6302f09fbd7c62d
yoonjinlee <yoonjinifer@gmail.com>
2021-01-09 04:35:06 +0900

    💄 유니버스 카드뷰 구성중

---
+++ Milkyway/Milkyway/Screens/MyUniverse/ViewControllers/UniverseCardVC.swift
@@ -0,0 +1,29 @@
+//
+//  UniverseCardVC.swift
+//  Milkyway
+//
+//  Created by 이윤진 on 2021/01/09.
+//
+
+import UIKit
+
+class UniverseCardVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

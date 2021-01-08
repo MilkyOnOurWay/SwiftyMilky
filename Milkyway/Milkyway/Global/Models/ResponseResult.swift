9a993c6748f3a1ab48d65bdef6c6d01d1fd7485c
yoonjinlee <yoonjinifer@gmail.com>
2021-01-09 01:14:02 +0900

    🚀 서버 파일 추가

---
+++ Milkyway/Milkyway/Global/Models/ResponseResult.swift
@@ -0,0 +1,28 @@
+//
+//  ResponseResult.swift
+//  Milkyway
+//
+//  Created by 이윤진 on 2021/01/09.
+//
+
+import Foundation
+
+struct ResponseResult<T: Codable>: Codable {
+    var status: Int?
+    var success: Bool?
+    var message: String?
+    var data: [T]?
+}
+
+struct ResponseSimpleResult<T: Codable>: Codable {
+    var status: Int?
+    var success: Bool?
+    var message: String?
+    var data: T?
+}
+
+struct ResponseTempResult: Codable {
+    var status: Int?
+    var success: Bool?
+    var message: String?
+}

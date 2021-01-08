b3d17154b0e9a6111e107d31e39289c4d4fcc35a
yoonjinlee <yoonjinifer@gmail.com>
2021-01-09 04:56:24 +0900

    📡 서버 response result 수정

--- Milkyway/Milkyway/Global/Models/ResponseResult.swift
+++ Milkyway/Milkyway/Global/Models/ResponseResult.swift
@@ -9,20 +9,17 @@ import Foundation
 
 struct ResponseResult<T: Codable>: Codable {
     var status: Int?
-    var success: Bool?
     var message: String?
     var data: [T]?
 }
 
 struct ResponseSimpleResult<T: Codable>: Codable {
     var status: Int?
-    //var success: Bool?
     var message: String?
     var data: T?
 }
 
 struct ResponseTempResult: Codable {
     var status: Int?
-    var success: Bool?
     var message: String?
 }

ced60314262bca09e1e2d8faa1ae2d894809779f
소영 <sso_0022@naver.com>
2021-01-09 00:01:07 +0900

    🎨 메뉴등록 입력완료버튼 색상바뀌게 완료

--- Milkyway/Milkyway/Screens/Report/CafeReport/ViewControllers/MenuPlusVC.swift
+++ Milkyway/Milkyway/Screens/Report/CafeReport/ViewControllers/MenuPlusVC.swift
@@ -19,14 +19,23 @@ class MenuPlusVC: UIViewController {
     var editCafeMenu = CafeMenu(menu: "", selection: [], price: "")
     var areyouEdit = false
     var category = [Int]()
+    
+    var menuCount = 0
+    var priceCount = 0
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        menuTF.delegate = self
+        priceTF.delegate = self
+        
         categoryFirstBtn.isMultipleSelectionEnabled = true
         readyToEdit()
         
+        // 처음에는 입력 완료를 누를 수 없다.
+        editEndBtn.isUserInteractionEnabled = false
+   
     }
 }
 
@@ -41,9 +50,8 @@ extension MenuPlusVC  {
         // 버튼을 눌러도 키보드가 내려간다네...
         self.menuTF.resignFirstResponder()
         self.priceTF.resignFirstResponder()
-        if menuTF.text != nil && priceTF.text != nil && category.count != 0 {
-            
-        }
+        countCategory()
+        checkEditOK()
         
     }
     
@@ -54,14 +62,7 @@ extension MenuPlusVC  {
             NotificationCenter.default.post(name: Notification.Name("remove"), object: nil)
         }
         
-        if categoryFirstBtn.isSelected {
-            category.append(1)
-        }
-        categoryFirstBtn.otherButtons.forEach{ i in
-            if i.isSelected {
-                category.append(i.tag)
-            }
-        }
+      
         print(category)
         print("\(menuTF.text!) 메뉴 추가합니다")
         
@@ -76,9 +77,66 @@ extension MenuPlusVC  {
         self.navigationController?.popViewController(animated: true)
     }
     
+
+    
+    // 텍스트필드 아닌 곳을 터치하면 키보드가 내려간다네 ...
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.menuTF.resignFirstResponder()
+        self.priceTF.resignFirstResponder()
+    }
     
     
+}
+
+
+
+extension MenuPlusVC: UITextFieldDelegate {
+    func  textFieldDidEndEditing(_ textField: UITextField) {
+        print(menuTF.text!.count)
+        print(priceTF.text!.count)
+        menuCount = menuTF.text!.count
+        priceCount = priceTF.text!.count
+        countCategory()
+        checkEditOK()
     
+    }
+        
+       
+    
+}
+
+
+extension MenuPlusVC {
+    
+    func checkEditOK() {
+        
+        if menuCount > 0 && priceCount > 0 && category.count > 0 {
+            editEndBtn.setImage(UIImage(named: "btnInput"), for: .normal)
+            editEndBtn.isUserInteractionEnabled = true
+        }
+        else {
+            editEndBtn.setImage(UIImage(named: "btnInputOff"), for: .normal)
+            editEndBtn.isUserInteractionEnabled = false
+        }
+        
+        
+    }
+    
+    func countCategory() {
+        category = []
+        if categoryFirstBtn.isSelected {
+            category.append(1)
+        }
+        categoryFirstBtn.otherButtons.forEach{ i in
+            if i.isSelected {
+                category.append(i.tag)
+            }
+        }
+        print(category)
+    }
+    
+    
+    // 수정하기 전에 필요한 작업
     func readyToEdit() {
         menuTF.text = editCafeMenu.menu
         priceTF.text = editCafeMenu.price
@@ -94,15 +152,4 @@ extension MenuPlusVC  {
             }
         }
     }
-    
-    
-    
-    // 텍스트필드 아닌 곳을 터치하면 키보드가 내려간다네 ...
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.menuTF.resignFirstResponder()
-        self.priceTF.resignFirstResponder()
-    }
 }
-
-
-

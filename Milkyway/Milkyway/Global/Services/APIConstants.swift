3eb9c560c35187f90a9b01b698456f15e61475b3
yoonjinlee <yoonjinifer@gmail.com>
2021-01-11 02:28:10 +0900

    🚀 서버 파일 수정

--- Milkyway/Milkyway/Global/Services/APIConstants.swift
+++ Milkyway/Milkyway/Global/Services/APIConstants.swift
@@ -12,6 +12,7 @@ struct APIConstants {
     // 일단 위키 기준으로 현재 나온 것들만 작성함.
     // 혹시 추가적으로 나온 API가 있다면 하단 예시를 참고하여 새롭게 생성해주면 됩니다.
     // 자신이 연결해야하는 API 변수명 마음에 안들면 바꿔도 상관없음.
+    // [참고] 주소에 나온 :idx에서 : 빼고 보내줘야함
     
     // POST 회원가입
     static let signUp = BaseURL + "users/signup"
@@ -35,7 +36,7 @@ struct APIConstants {
     static let deleteUniverse = BaseURL + "universe/"
     
     // GET 주변 마이유니버스 불러오기
-    
+    static let showUniverse = BaseURL + "universe/universeHome"
     // GET 카페 상세 정보 불러오기
     static let loadCafeInfo = BaseURL + "cafe/"
     
@@ -46,11 +47,13 @@ struct APIConstants {
     static let deleteCafeInfo = BaseURL + "report/"
     
     // POST 카페 제보
+    static let reportCafe = BaseURL + "report/"
     
     // GET 내가 제보한 카페 불러오기(나의제보)
+    static let showMyReport = BaseURL + "report"
     
     // GET 제보한 카페 거절 이유
     
-    // DELETE 제보한 카페 확인 후 삭제
-    
+    // DELETE 제보한 카페 확인 후 삭제 report/:cafeId
+    static let deleteReport = BaseURL + "report/"
 }

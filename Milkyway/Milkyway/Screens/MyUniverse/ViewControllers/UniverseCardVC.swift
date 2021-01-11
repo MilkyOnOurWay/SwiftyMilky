5f68cb490523f16cbc7cd92a4f9dc6d56d9aca7c
yoonjinlee <yoonjinifer@gmail.com>
2021-01-11 15:30:44 +0900

    ⚡️ 유니버스 카드뷰 라운딩 처리

--- Milkyway/Milkyway/Screens/MyUniverse/ViewControllers/UniverseCardVC.swift
+++ Milkyway/Milkyway/Screens/MyUniverse/ViewControllers/UniverseCardVC.swift
@@ -8,28 +8,35 @@
 import UIKit
 
 class UniverseCardVC: UIViewController {
-
+    
     
     @IBOutlet weak var cafeNameLabel: UILabel!
     @IBOutlet weak var cafeAddressLabel: UILabel!
     @IBOutlet weak var cafeTimeLabel: UILabel!
     @IBOutlet weak var DeleteButton: UIButton!
+    @IBOutlet var rootView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setDeleteButton()
         setView()
         // Do any additional setup after loading the view.
     }
-
-
-   
+    
+    
+    
 }
 
 extension UniverseCardVC {
     
     func setView(){
         
-        //rootView.layer.cornerRadius = 12
+        rootView.layer.cornerRadius = 12
+        rootView.layer.shadowColor = UIColor.black.cgColor
+        rootView.layer.shadowOpacity = 0.1
+        rootView.layer.shadowOffset = CGSize(width: 0.0, height: -4.0)
+        rootView.layer.shadowRadius = 12
+        rootView.layer.masksToBounds = false
         
     }
     func setDeleteButton(){

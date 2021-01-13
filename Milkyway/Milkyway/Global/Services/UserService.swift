4b1baa2ad721b71a981b4a3f9bd8c26f1c24053e
yoonjinlee <yoonjinifer@gmail.com>
2021-01-14 04:21:38 +0900

    📡 닉네임 변경 API 연결 진행중

--- Milkyway/Milkyway/Global/Services/UserService.swift
+++ Milkyway/Milkyway/Global/Services/UserService.swift
@@ -67,4 +67,66 @@ struct UserService {
     }
     
     // MARK: - 닉네임 변경 API 연결
+    
+    func changeNickname(_ nickName: String, completion: @escaping (NetworkResult<Any>) -> Void) {
+        let URL = APIConstants.changeNick
+        let headers: HTTPHeaders = [
+            "Content-Type": "application/json",
+            "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWR4Ijo1LCJpYXQiOjE2MDk3Nzg0NjksImV4cCI6MTYxMjM3MDQ2OSwiaXNzIjoibWlsa3lXYXkifQ.c2JAdyd0pGQzbmT0E_yl51eAGkcO71YfokwJebqqDME"
+        ]
+        
+        let body: Parameters = [
+            
+            "newNickName": nickName
+        ]
+        
+        let dataRequest = AF.request(URL,
+                                     method: .put,
+                                     parameters: body,
+                                     encoding: JSONEncoding.default,
+                                     headers: headers)
+        
+        dataRequest.responseData { (response) in
+            
+            switch response.result {
+            
+            case .success(_):
+                if let value = response.value {
+                    
+                    if let status = response.response?.statusCode {
+                        
+                        print(status)
+                        switch status {
+                        case 200:
+                            do {
+                                let decoder = JSONDecoder()
+                                let result = try decoder.decode(ResponseTempResult.self,from: value)
+                                
+                            } catch {
+                                completion(.pathErr)
+                            }
+                        case 400:
+                            do{
+                                let decoder = JSONDecoder()
+                                let result = try decoder.decode(ResponseTempResult.self,
+                                                                from: value)
+                                
+                                completion(.requestErr(result.message))
+                            } catch {
+                                completion(.pathErr)
+                            }
+                        case 500:
+                            completion(.serverErr)
+                        default:
+                            break
+                        }
+                    }
+                }
+            case .failure(_):
+                completion(.networkFail)
+            }
+        }
+    }
+    
+    
 }
--- Milkyway/Milkyway/Screens/Setting/ViewControllers/NicknameChangeVC.swift
+++ Milkyway/Milkyway/Screens/Setting/ViewControllers/NicknameChangeVC.swift
@@ -62,7 +62,7 @@ extension NicknameChangeVC {
         changeButton.isEnabled = false
     }
     
-  
+    
     func isValidNickname(_ nickname: String) -> Bool{
         
         let nicknameRegEx = "[가-힣0-9]{2,10}"
@@ -111,11 +111,37 @@ extension NicknameChangeVC {
     @objc func changeButtonDidTap(){
         
         // 여기서 서버코드 연결할 것
-        let vc = UIStoryboard.init(name: "TabBar", bundle: nil).instantiateViewController(identifier: "TabBarController") as? TabBarController
-        vc?.modalPresentationStyle = .fullScreen
-        self.present(vc!, animated: true, completion: nil)
+//        let vc = UIStoryboard.init(name: "TabBar", bundle: nil).instantiateViewController(identifier: "TabBarController") as? TabBarController
+//        vc?.modalPresentationStyle = .fullScreen
+//        self.present(vc!, animated: true, completion: nil)
+       changeNicknameService(nickname ?? "")
     }
     
+    // MARK: - 닉네임 변경 서버
+    
+    func changeNicknameService(_ nickname: String){
+        
+        UserService.shared.changeNickname(nickname) {
+            responseData in
+            switch responseData {
+            case .success(let res):
+                dump(res)
+                let response = res as! ResponseTempResult
+                
+                let vc = UIStoryboard.init(name: "TabBar", bundle: nil).instantiateViewController(identifier: "TabBarController") as? TabBarController
+                vc?.modalPresentationStyle = .fullScreen
+                self.present(vc!, animated: true, completion: nil)
+            case .requestErr(_):
+                print(".requestErr")
+            case .pathErr:
+                print(".pathErr")
+            case .serverErr:
+                print(".serverErr")
+            case .networkFail:
+                print(".networkErr")
+            }
+        }
+    }
     
 }
 
@@ -124,62 +150,62 @@ extension NicknameChangeVC: UITextFieldDelegate {
     
     // MARK: - 닉네임 유효성 판단
     @objc func checkRegister(_ textfield: UITextField) {
-    
-    nickname = textfield.text
-    let count = String(nickname?.count ?? 0)
-    countLabel.text = "\(count)/10"
-    
-    if nickname?.count ?? 0 > 10 {
-    countLabel.isHidden = true
-    }
-    
-    if !isValidNickname(nickname!){
-    stateLabel.isHidden = false
-    countLabel.isHidden = false
-    if nickname?.count == 0 {
-    countLabel.isHidden = true
-    }
-    checkBoxImageView.isHidden = true
-    changeButton.setBackgroundImage(UIImage(named: "btnStartDisabled"), for: .normal)
-    stateLabel.text = "사용 불가한 닉네임입니다."
-    stateLabel.textColor = .red
-    backgroundImageView.image = UIImage(named: "inputError")
-    return
-    }else {
-    
-    stateLabel.isHidden = true
-    changeButton.setBackgroundImage(UIImage(named: "btnStart"), for: .normal)
-    changeButton.isEnabled = true
-    checkBoxImageView.isHidden = false
-    backgroundImageView.image = UIImage(named: "inputCorrect")
-    
-    return
-    }
-    
-    
+        
+        nickname = textfield.text
+        let count = String(nickname?.count ?? 0)
+        countLabel.text = "\(count)/10"
+        
+        if nickname?.count ?? 0 > 10 {
+            countLabel.isHidden = true
+        }
+        
+        if !isValidNickname(nickname!){
+            stateLabel.isHidden = false
+            countLabel.isHidden = false
+            if nickname?.count == 0 {
+                countLabel.isHidden = true
+            }
+            checkBoxImageView.isHidden = true
+            changeButton.setBackgroundImage(UIImage(named: "btnStartDisabled"), for: .normal)
+            stateLabel.text = "사용 불가한 닉네임입니다."
+            stateLabel.textColor = .red
+            backgroundImageView.image = UIImage(named: "inputError")
+            return
+        }else {
+            
+            stateLabel.isHidden = true
+            changeButton.setBackgroundImage(UIImage(named: "btnStart"), for: .normal)
+            changeButton.isEnabled = true
+            checkBoxImageView.isHidden = false
+            backgroundImageView.image = UIImage(named: "inputCorrect")
+            
+            return
+        }
+        
+        
     }
     
     func checkMaxLength(_ textfield: UITextField, maxLength: Int){
-    if(textfield.text?.count ?? 0 > maxLength){
-    textfield.deleteBackward()
-    textfield.resignFirstResponder()
-    }
+        if(textfield.text?.count ?? 0 > maxLength){
+            textfield.deleteBackward()
+            textfield.resignFirstResponder()
+        }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-    self.view.endEditing(true)
+        self.view.endEditing(true)
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-    
-    textField.resignFirstResponder()
-    return true
+        
+        textField.resignFirstResponder()
+        return true
     }
     
     
     
-    }
-    
+}
+
 
 
 
